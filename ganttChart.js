00ee19a815967a34fbf60a2194cd2c9b14109a98
vishwapkm-ey <59816351+vishwapkm-ey@users.noreply.github.com>
2020-01-13 13:53:30 +0530

    Update ganttChart.js

--- ganttChart.js
+++ ganttChart.js
@@ -1,8 +1,7 @@
 (function() { 
 	let template = document.createElement("template");
 	template.innerHTML = `
-		<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-		
+		<div id='text_div'>Gantt Chart</div>
 		<div id="chart_div"></div>		  
 	`;
 
@@ -12,6 +11,8 @@
 			let shadowRoot = this.attachShadow({mode: "open"});
 			shadowRoot.appendChild(template.content.cloneNode(true));
 			
+			this.$div = shadowRoot.querySelector('svg');
+			
 			this.addEventListener("click", event => {
 				var event = new Event("onClick");
 				this.dispatchEvent(event);
@@ -20,23 +21,8 @@
 			this._props = {};
 		}
 		
-		drawChart(gcData) {
-      			var data = new google.visualization.DataTable(gcData);
-      			
-      			var options = {
-        			height: 500
-      			};
-
-      			var chart = new google.visualization.Gantt(document.getElementById('chart_div'));
-
-      			chart.draw(data, options);
-    		}
-
 		render(gcData) {
-			google.charts.load('current', {'packages':['gantt']});
-    			google.charts.setOnLoadCallback(drawChart);
-
-    			this.drawChart(drawChart);
+			this.$div.innerHTML = '<text id="gcData_text">' + gcData + '</text>'
     		}
 		  
 		onCustomWidgetBeforeUpdate(changedProperties) {
