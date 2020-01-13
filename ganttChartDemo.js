5c8427ee25a0478ea3fe109dc87f027dd49c1cc1
vishwapkm-ey <59816351+vishwapkm-ey@users.noreply.github.com>
2020-01-13 14:37:20 +0530

    Create ganttChartDemo.js

---
+++ ganttChartDemo.js
@@ -0,0 +1,45 @@
+(function() { 
+	let template = document.createElement("template");
+	template.innerHTML = `
+		<div class="container">
+		  <div class="row">
+		    <svg viewBox="0 0 1000 500">
+			  </svg>
+		   </div>
+		</div>
+	`;
+
+	class GanttChart extends HTMLElement {
+		constructor() {
+			super(); 
+			let shadowRoot = this.attachShadow({mode: "open"});
+			shadowRoot.appendChild(template.content.cloneNode(true));
+			
+			this.$svg = shadowRoot.querySelector('svg');
+			
+			this.addEventListener("click", event => {
+				var event = new Event("onClick");
+				this.dispatchEvent(event);
+			});
+			
+			this._props = {};
+		}
+		
+		render(val) {
+			this.$svg.innerHTML = '<text class="percentage" text-anchor="middle" alignment-baseline="middle" font-size="140" font-weight="bold">' + val + '</text>';
+	  }
+		  
+		onCustomWidgetBeforeUpdate(changedProperties) {
+			this._props = { ...this._props, ...changedProperties };
+		}
+
+		onCustomWidgetAfterUpdate(changedProperties) {
+			if ("value" in changedProperties) {
+				this.$value = changedProperties["value"];
+			}
+      this.render(this.$value);
+		}
+	}
+	
+	customElements.define("com-demo-gantt", GanttChart);
+})();
