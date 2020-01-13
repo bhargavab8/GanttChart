fbf2a049d89b5d705f9b5efb41df24ad24cb09e5
vishwapkm-ey <59816351+vishwapkm-ey@users.noreply.github.com>
2020-01-13 13:20:24 +0530

    Add files via upload

---
+++ ganttChart.js
@@ -0,0 +1,59 @@
+(function() { 
+	let template = document.createElement("template");
+	template.innerHTML = `
+		<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+		
+		<div id="chart_div"></div>		  
+	`;
+
+	class GanttChart extends HTMLElement {
+		constructor() {
+			super(); 
+			let shadowRoot = this.attachShadow({mode: "open"});
+			shadowRoot.appendChild(template.content.cloneNode(true));
+			
+			this.$script = shadowRoot.querySelector('script');			
+			this.$svg = shadowRoot.querySelector('chart_div');
+			
+			this.addEventListener("click", event => {
+				var event = new Event("onClick");
+				this.dispatchEvent(event);
+			});
+			
+			this._props = {};
+		}
+		
+		drawChart(gcData) {
+      			var data = new google.visualization.DataTable(gcData);
+      			
+      			var options = {
+        			height: 500
+      			};
+
+      			var chart = new google.visualization.Gantt(document.getElementById('chart_div'));
+
+      			chart.draw(data, options);
+    		}
+
+		render(gcData) {
+			google.charts.load('current', {'packages':['gantt']});
+    			google.charts.setOnLoadCallback(drawChart);
+
+    			this.drawChart(drawChart);
+    		}
+		  
+		onCustomWidgetBeforeUpdate(changedProperties) {
+			this._props = { ...this._props, ...changedProperties };
+		}
+
+		onCustomWidgetAfterUpdate(changedProperties) {
+			if ("gcData" in changedProperties) {
+				this.$gcData = changedProperties["gcData"];
+			}
+			
+			this.render(this.$gcData);
+		}
+	}
+	
+	customElements.define("com-demo-gantt", GanttChart);
+})();
