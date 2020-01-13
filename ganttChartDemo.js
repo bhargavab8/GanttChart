761f110c50d2e65d51f2c8a75abf35bc590b7441
vishwapkm-ey <59816351+vishwapkm-ey@users.noreply.github.com>
2020-01-13 17:09:15 +0530

    Update ganttChartDemo.js

--- ganttChartDemo.js
+++ ganttChartDemo.js
@@ -30,7 +30,32 @@
     				google.charts.load('current', {'packages':['gantt']});
 				
 				// Set a callback to run when the Google Visualization API is loaded.
-    				google.charts.setOnLoadCallback(drawChart);				
+    				google.charts.setOnLoadCallback(function() {
+					var data = new google.visualization.DataTable();
+      					data.addColumn('string', 'Task ID');
+      					data.addColumn('string', 'Task Name');
+      					data.addColumn('date', 'Start Date');
+	      				data.addColumn('date', 'End Date');
+      					data.addColumn('number', 'Duration');
+      					data.addColumn('number', 'Percent Complete');
+      					data.addColumn('string', 'Dependencies');
+
+      					data.addRows([
+        					['Research', 'Find sources', new Date(2015, 0, 1), new Date(2015, 0, 5), null,  100,  null],
+        					['Write', 'Write paper', null, new Date(2015, 0, 9), daysToMilliseconds(3), 25, 'Research,Outline'],
+          					['Cite', 'Create bibliography', null, new Date(2015, 0, 7), daysToMilliseconds(1), 20, 'Research'],
+        					['Complete', 'Hand in paper', null, new Date(2015, 0, 10), daysToMilliseconds(1), 0, 'Cite,Write'],
+        					['Outline', 'Outline paper', null, new Date(2015, 0, 6), daysToMilliseconds(1), 100, 'Research']
+      					]);
+
+      					var options = {
+        					height: 275
+      					};
+
+      					var chart = new google.visualization.Gantt(document.querySelector('.ganttContainer'));
+
+      					chart.draw(data, options);
+				});	
 			}
 			script.src = 'https://www.gstatic.com/charts/loader.js';
 
@@ -43,30 +68,7 @@
     		}
 		
 		drawChart(){
-			var data = new google.visualization.DataTable();
-      			data.addColumn('string', 'Task ID');
-      			data.addColumn('string', 'Task Name');
-      			data.addColumn('date', 'Start Date');
-	      		data.addColumn('date', 'End Date');
-      			data.addColumn('number', 'Duration');
-      			data.addColumn('number', 'Percent Complete');
-      			data.addColumn('string', 'Dependencies');
-
-      			data.addRows([
-        			['Research', 'Find sources', new Date(2015, 0, 1), new Date(2015, 0, 5), null,  100,  null],
-        			['Write', 'Write paper', null, new Date(2015, 0, 9), daysToMilliseconds(3), 25, 'Research,Outline'],
-          			['Cite', 'Create bibliography', null, new Date(2015, 0, 7), daysToMilliseconds(1), 20, 'Research'],
-        			['Complete', 'Hand in paper', null, new Date(2015, 0, 10), daysToMilliseconds(1), 0, 'Cite,Write'],
-        			['Outline', 'Outline paper', null, new Date(2015, 0, 6), daysToMilliseconds(1), 100, 'Research']
-      			]);
-
-      			var options = {
-        			height: 275
-      			};
-
-      			var chart = new google.visualization.Gantt(document.querySelector('.ganttContainer'));
-
-      			chart.draw(data, options);
+			
 		}
 		  
 		onCustomWidgetBeforeUpdate(changedProperties) {
