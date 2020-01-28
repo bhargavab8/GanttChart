1b7aaa929857227e6e8804d98716a268ee68f65c
vishwapkm-ey <59816351+vishwapkm-ey@users.noreply.github.com>
2020-01-28 13:30:09 +0530

    Update ganttChartDemoNew.js

--- ganttChartDemoNew.js
+++ ganttChartDemoNew.js
@@ -96,7 +96,7 @@
 				    alert(data.wg[selection[0].row].c[6].v);
 				    perComp = data.wg[selection[0].row].c[6].v;	
 				    var selValuePlaceholder = document.querySelector(".sapCustomWidgetWebComponent").shadowRoot.querySelector("#selectedBar");	
-				    selValuePlaceholder.innerHTML(perComp);					
+				    selValuePlaceholder.innerText(perComp);					
 				}				
 			    });		
                         });                            
@@ -173,7 +173,7 @@
 			    alert(data.wg[selection[0].row].c[6].v);
 			    perComp = data.wg[selection[0].row].c[6].v;
 			    var selValuePlaceholder = document.querySelector(".sapCustomWidgetWebComponent").shadowRoot.querySelector("#selectedBar");	
-			    selValuePlaceholder.innerHTML(perComp);	
+			    selValuePlaceholder.innerText(perComp);	
 			}
 		   });
                });		    
